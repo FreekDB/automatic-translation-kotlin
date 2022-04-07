76d1b3fbb291b84b19f0ee6f412b07c40e6939c3
Freek de Bruijn <freekb@topdesk.com>
2022-04-07 22:01:21 +0200

    Added build files.

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val kotlinVersion: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.20"
+    kotlin("plugin.serialization") version "1.6.20"
+}
+
+group = "com.github"
+version = "0.0.1"
+application {
+    mainClass.set("com.github.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
